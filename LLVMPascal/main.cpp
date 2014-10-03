bfc1cb628776648406228f2832ca6dd95da2b3ea
yhfyhf <yhf406716870@gmail.com>
2014-10-03 22:17:22 +0800

    Initial Commit

---
+++ LLVMPascal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LLVMPascal
+//
+//  Created by yhf on 10/3/14.
+//  Copyright (c) 2014 yhf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
